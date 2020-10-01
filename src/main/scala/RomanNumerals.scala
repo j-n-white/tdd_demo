07c2d75a9cf49339833f1bf99825b3c23cf59ee2
James White <jwhite@scottlogic.com>
2020-10-01 15:41:06 +0100

    14 - Green - passing for 4

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -3,7 +3,8 @@ object RomanNumerals {
   implicit class ToRomanNumerals(self: Int) {
     def toRomanNumerals: String = {
       val sb = new StringBuilder()
-      for (_ <- 0 until self) sb.append("I")
+      if (4 == self) sb.append("IV")
+      else for (_ <- 0 until self) sb.append("I")
       sb.toString()
     }
   }
