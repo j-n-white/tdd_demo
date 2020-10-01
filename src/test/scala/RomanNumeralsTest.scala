05c2c8a7a69de2f4798e1e7b4938912abd6a81a6
James White <jwhite@scottlogic.com>
2020-10-01 17:00:48 +0100

    22 - Red - test for 8

--- src/test/scala/RomanNumeralsTest.scala
+++ src/test/scala/RomanNumeralsTest.scala
@@ -11,7 +11,8 @@ class RomanNumeralsTest extends AnyPropSpec with TableDrivenPropertyChecks {
     (4, "IV"),
     (5, "V"),
     (6, "VI"),
-    (7, "VII")
+    (7, "VII"),
+    (8, "VIII")
   )
   property("Given the toRomanNumerals function when called with an integer it returns the correct string") {
     forAll(testCases) { (input: Int, expected: String) => assertResult(expected)(input.toRomanNumerals) }
