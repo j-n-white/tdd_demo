5d8c61494ca942017d077490d6c61febd9257252
James White <jwhite@scottlogic.com>
2020-10-01 15:27:02 +0100

    11 - Green - passing for 3

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -2,7 +2,7 @@ object RomanNumerals {
 
   implicit class ToRomanNumerals(self: Int) {
     def toRomanNumerals:String = {
-      if (self == 1) "I" else "II"
+      if (self == 1) "I" else if (self == 2) "II"else "III"
     }
   }
 
