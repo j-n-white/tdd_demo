6fb06763dd124f49714d67579ba2643b21c219de
James White <jwhite@scottlogic.com>
2020-10-01 15:37:36 +0100

    12 - Refactor - use loop and tidy git ignore file

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -1,8 +1,10 @@
 object RomanNumerals {
 
   implicit class ToRomanNumerals(self: Int) {
-    def toRomanNumerals:String = {
-      if (self == 1) "I" else if (self == 2) "II"else "III"
+    def toRomanNumerals: String = {
+      val sb = new StringBuilder()
+      for (_ <- 0 until self) sb.append("I")
+      sb.toString()
     }
   }
 
