e9dbd6880520372b1fdc2311095d8d5bc4538039
James White <jwhite@scottlogic.com>
2020-10-01 14:24:37 +0100

    06 - Refactor - remove redundant test

--- src/test/scala/RomanNumeralsTest.scala
+++ src/test/scala/RomanNumeralsTest.scala
@@ -3,11 +3,8 @@ import org.scalatest.funspec.AnyFunSpec
 
 class RomanNumeralsTest extends AnyFunSpec {
   describe("Given the toRomanNumerals function") {
-    it("when called then it returns a string") {
-      assert(1.toRomanNumerals.isInstanceOf[String])
-    }
     it("when called on 1 then it returns \"I\"") {
-      assert(1.toRomanNumerals.eq("I"))
+      assertResult("I")(1.toRomanNumerals)
     }
   }
 }
