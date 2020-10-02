c6564c959a3cf1becb179144b3dc3d0d40c101e9
James White <jwhite@scottlogic.com>
2020-10-02 13:55:27 +0100

    46 - Red - test for 400

--- src/test/scala/RomanNumeralsTest.scala
+++ src/test/scala/RomanNumeralsTest.scala
@@ -24,7 +24,8 @@ class RomanNumeralsTest extends AnyPropSpec with TableDrivenPropertyChecks {
     (40, "XL"),
     (50, "L"),
     (90, "XC"),
-    (100, "C")
+    (100, "C"),
+    (400, "CD")
   )
   property("Given the toRomanNumerals function when called with an integer it returns the correct string") {
     forAll(testCases) { (input: Int, expected: String) => assertResult(expected)(input.toRomanNumerals) }
