bef122169f96b9a566fa186f92b79d89234544c6
James White <jwhite@scottlogic.com>
2020-10-02 10:27:40 +0100

    26 - Green - passing for 9

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -3,13 +3,17 @@ object RomanNumerals {
   implicit class ToRomanNumerals(self: Int) {
     def toRomanNumerals: String = {
       val sb = new StringBuilder()
-      var remaining = self;
-      if (remaining >= 5) {
-        sb.append("V")
-        remaining -= 5
+      if (self == 9) {
+        sb.append("IX")
+      } else {
+        var remaining = self;
+        if (remaining >= 5) {
+          sb.append("V")
+          remaining -= 5
+        }
+        if (4 == remaining) sb.append("IV")
+        else for (_ <- 0 until remaining) sb.append("I")
       }
-      if (4 == remaining) sb.append("IV")
-      else for (_ <- 0 until remaining) sb.append("I")
       sb.toString()
     }
   }
