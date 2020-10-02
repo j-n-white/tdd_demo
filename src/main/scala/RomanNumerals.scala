2c26677fa2bffc3e2a491f856a3c1f8fdf3a24a9
James White <jwhite@scottlogic.com>
2020-10-02 13:38:19 +0100

    38 - Green - passing for 40

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -5,17 +5,18 @@ object RomanNumerals {
       val sb = new StringBuilder()
       var remaining = self
 
-      def appendCharacterForNumber(char: Char, number: Int): Unit = {
+      def appendCharacterForNumber(char: String, number: Int): Unit = {
         while (remaining >= number) {
           sb.append(char)
           remaining -= number
         }
       }
 
-      appendCharacterForNumber('X', 10)
+      appendCharacterForNumber("XL", 40)
+      appendCharacterForNumber("X", 10)
       if (remaining == 9) sb.append("IX")
       else {
-        appendCharacterForNumber('V', 5)
+        appendCharacterForNumber("V", 5)
         if (4 == remaining) sb.append("IV")
         else for (_ <- 0 until remaining) sb.append("I")
       }
